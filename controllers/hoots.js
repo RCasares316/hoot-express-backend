50cd21a0b6efe90e9ec29f1ffa9909479351d949
RCasares316 <richiecasares@gmail.com>
2026-02-06 15:27:22 -0500

    1/3 of controllers hoots

--- controllers/hoots.js
+++ controllers/hoots.js
@@ -1,26 +1,32 @@
-import Hoot from "../models/hoot.js"
+import Hoot from "../models/hoot.js";
 
-export const getHoots = async () => {
+export const getHoots = async () => {};
 
-}
+export const getHoot = async () => {};
 
-export const getHoot = async () => {
-    
-}
+export const createHoot = async () => {};
 
-export const createHoot = async () => {
-    
-}
+export const updateHoot = async () => {};
 
-export const updateHoot = async () => {
-    
-}
+export const deleteHoot = async (req, res) => {
+  try {
+    const { hootId } = req.params;
 
-export const deleteHoot = async () => {
-    
-}
+    const deleted = await Hoot.findByIdAndDelete(hootId);
 
-export const addComment = async () => {
-    
-}
+    if (!deleted) {
+      res.status(404);
+      throw new Error("Hoot not found.");
+    }
 
+    res.status(200).json(deleted);
+  } catch (error) {
+    if (res.statusCode === 404) {
+      res.json({ err: err.message });
+    } else {
+      res.status(500).json({ err: err.message });
+    }
+  }
+};
+
+export const addComment = async (req, res) => {};
