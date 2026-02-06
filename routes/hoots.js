50d52b50a5bbd51421eb444a6dc16117b1278295
RCasares316 <richiecasares@gmail.com>
2026-02-06 15:00:20 -0500

    Working on routes

--- routes/hoots.js
+++ routes/hoots.js
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import verifyToken from "../middleware/verify-token.js";
+import * as hootControllers from "../controllers/hoots.js"
 
 const router = Router()
 
@@ -8,7 +9,7 @@ router.get("/", verifyToken)
 router.get("/:hootId", verifyToken)
 router.put("/:hootId", verifyToken,)
 router.delete("/:hootId", verifyToken)
-router.post("/", verifyToken)
+router.post("/:hootId/comments", verifyToken)
 
 
 export default router
