e0a9db98491308ffa013042607fe177e74afdbf6
JEStudyhall <jaieelizabeth@gmail.com>
2026-02-06 15:29:35 -0500

    updated controller

--- controllers/hoots.js
+++ controllers/hoots.js
@@ -1,26 +1,30 @@
-import Hoot from "../models/hoot.js"
+import Hoot from "../models/hoot.js";
 
-export const getHoots = async () => {
+export const getHoots = async () => {};
 
-}
+export const getHoot = async () => {};
 
-export const getHoot = async () => {
-    
-}
+export const createHoot = async (req, res) => {
+  try {
+    const newHoot = await Hoot.create(req.body);
+    res.json(newHoot);
+  } catch (error) {
+    console.log(Error);
+  }
+};
 
-export const createHoot = async () => {
-    
-}
+export const updateHoot = async (req, res) => {
+  try {
+    const updatedHoot = await Hoot.findByIdAndUpdate(
+      req.params.hootId,
+      req.body,
+    );
+    res.json(updatedHoot);
+  } catch (error) {
+    console.log(Error);
+  }
+};
 
-export const updateHoot = async () => {
-    
-}
+export const deleteHoot = async () => {};
 
-export const deleteHoot = async () => {
-    
-}
-
-export const addComment = async () => {
-    
-}
-
+export const addComment = async () => {};
