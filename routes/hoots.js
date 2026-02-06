ee629d86214c6a1e1088b0c91c8c26d0c9d9d39e
RCasares316 <richiecasares@gmail.com>
2026-02-06 15:04:11 -0500

    routes completed

--- routes/hoots.js
+++ routes/hoots.js
@@ -1,15 +1,14 @@
 import { Router } from "express";
 import verifyToken from "../middleware/verify-token.js";
-import * as hootControllers from "../controllers/hoots.js"
+import * as hootControllers from "../controllers/hoots.js";
 
-const router = Router()
+const router = Router();
 
-router.post("/", verifyToken)
-router.get("/", verifyToken)
-router.get("/:hootId", verifyToken)
-router.put("/:hootId", verifyToken,)
-router.delete("/:hootId", verifyToken)
-router.post("/:hootId/comments", verifyToken)
+router.post("/", verifyToken, hootControllers.createHoot);
+router.get("/", verifyToken, hootControllers.getHoots);
+router.get("/:hootId", verifyToken, hootControllers.getHoot);
+router.put("/:hootId", verifyToken, hootControllers.updateHoot);
+router.delete("/:hootId", verifyToken, hootControllers.deleteHoot);
+router.post("/:hootId/comments", verifyToken, hootControllers.addComment);
 
-
-export default router
+export default router;
