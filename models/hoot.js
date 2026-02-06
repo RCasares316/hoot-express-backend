4993e38b782adba8b39d60f36d9cd41fa0ccc201
JEStudyhall <jaieelizabeth@gmail.com>
2026-02-06 14:52:54 -0500

    hootSchema Completed

---
+++ models/hoot.js
@@ -0,0 +1,34 @@
+import mongoose from "mongoose";
+
+const commentSchema = new mongoose.Schema({
+  text: {
+    type: String,
+    required: true,
+  },
+  author: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+});
+
+const hootSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  text: {
+    type: String,
+    required: true,
+  },
+  category: {
+    type: String,
+    required: true,
+  },
+  author: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+  comments: [commentSchema],
+});
+
+export default mongoose.model("Hoot", hootSchema);
